59c84f43d2bfb1fc2e6617324ba28f01ddfe298a
Mustafa <mustafakhericha89@gmail.com>
2025-01-29 18:33:52 +0530

    final workk done

--- main.py
+++ main.py
@@ -1,90 +1,97 @@
 import sys
 
-''' 
-    features
-        1. id pass
-        2. category
-        3. time slot
-        4. fees 
-        5. penalty
-        6. bill
-        7. bye bye come back msg
-''' 
-
 print('\t\tWelcome to Mk Library')
 
+# Global variables initialization
+sciFil = []
+romancel = []
+moneyl = []
+log = {}
 
-#id pass function
+# ID pass function
 def login():
     global log
     name = input('Enter your name: ')
     passw = input('Enter password: ')
-    log={name:passw}
-    print('Login successfull')
+    log = {name: passw}
+    print('Login successful')
 
-#category
+# Category selection function
 def cat():
+    global sciFil, romancel, moneyl
     while True:
-        print('List of books category available')
+        print('\nList of books category available')
         category = int(input('1.Sci-Fi\n2.Romance\n3.Money(recommended)\n4.Exit\n'))
-        match category:
-            case 1:
-                global sciFil
-                sciFil = []
-                l = int(input('How many books do you want in Sci-Fi?: '))
-                for i in range(l):
-                    sciFi = input('Enter which book do you want: ')
-                    sciFil.append(sciFi)
-                    print(f'{sciFi} book added to the bill\n')
-            case 2:
-                global romancel
-                romancel = []
-                l = int(input('How many books do you want in romance?: '))
-                for i in range(l):
-                    romance = input('Enter which book do you want: ')
-                    romancel.append(romance)
-                    print(f'{romance} book added to the bill\n')
-            case 3:
-                global moneyl
-                moneyl = []
-                l = int(input('How many books do you want in money oriented?: '))
-                for i in range(l):
-                    money = input('Enter which book do you want: ')
-                    moneyl.append(money)
-                    print(f'{money} book added to the bill\n')
-            case 4:
-                break
+        if category == 4:
+            break
+            
+        if category == 1:
+            l = int(input('How many books do you want in Sci-Fi?: '))
+            for _ in range(l):
+                book = input('Enter which book do you want: ')
+                sciFil.append(book)
+                print(f'{book} book added to the bill\n')
+                
+        elif category == 2:
+            l = int(input('How many books do you want in romance?: '))
+            for _ in range(l):
+                book = input('Enter which book do you want: ')
+                romancel.append(book)
+                print(f'{book} book added to the bill\n')
+                
+        elif category == 3:
+            l = int(input('How many books do you want in money oriented?: '))
+            for _ in range(l):
+                book = input('Enter which book do you want: ')
+                moneyl.append(book)
+                print(f'{book} book added to the bill\n')
 
-
-#time
+# Billing function
 def timme():
+    global sciFil, romancel, moneyl
     while True:
-        timee = int(input('Press 1 for purchase\n2 for rent: '))
+        timee = int(input('\nPress 1 for purchase\n2 for rent: '))
+        total_books = len(sciFil) + len(romancel) + len(moneyl)
+        
         if timee == 1:
-            sciFil = [0]
-            romancel = [0]
-            moneyl = [0]
-            books = (len(moneyl) + len(romancel) + len(sciFil))-3
-            if books > 0 and books <= 2:
-                bill = books*300
-            elif  books > 2 and books <= 4:
-                bill = books*250
+            if total_books == 0:
+                print("You haven't selected any books!")
+                continue
+                
+            if total_books <= 2:
+                bill = total_books * 300
+            elif total_books <= 4:
+                bill = total_books * 250
             else:
-                bill = books*220
-            print(f'Your total Bill {bill}Rs.')
+                bill = total_books * 220
+            print(f'Your total Purchase Bill: {bill} Rs.')
+            break
+            
         elif timee == 2:
-            books = (len(moneyl) + len(romancel) + len(sciFil))-3
-            if books > 0 and books <= 2:
-                bill = books*100
-            elif  books > 2 and books <= 4:
-                bill = books*75
+            if total_books == 0:
+                print("You haven't selected any books!")
+                continue
+                
+            if total_books <= 2:
+                bill = total_books * 100
+            elif total_books <= 4:
+                bill = total_books * 75
             else:
-                bill = books*50
-            print('Penalty Charges: 100Rs. per day')
-            print(f'Your rental Bill {bill}Rs.')
+                bill = total_books * 50
+            print('\nPenalty Charges: 100 Rs. per day')
+            print(f'Your Rental Bill: {bill} Rs.')
+            break
+            
         else:
-            print('Invalid Number Try Again!')
+            print('Invalid Number! Try Again!')
 
+# Farewell message
+def goodbye():
+    print('\nThank you for visiting Mk Library!')
+    print('Please come back again!')
+
+# Main program flow
 login()
 cat()
 timme()
+goodbye()
